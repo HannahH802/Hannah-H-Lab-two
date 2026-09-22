c758fec6d0d40403cb16c513b24cd15ce19a9ce1
HannahH802 <hhornyak29@setoncatholic.org>
2026-09-22 20:18:50 +0000

    Extra credit commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import random
 
 game_number = random.randint(1,10)
-print(game_number)
+guess_count = 0
 
 while(True):
 
     guess = int(input("Guess a number between 1 and 10: "))
+    guess_count += 1
 
     if guess > game_number:
         print("Boooo too high")
     elif guess < game_number: 
         print("Boooo too low")
     else:
-        print("You win!!")
+        if guess_count == 1:
+            comment = "Amazing! You got it on the first try!"
+        elif guess_count <= 3:
+            comment = "Great job! You were quick."
+        elif guess_count <= 6:
+            comment = "Nice work! You got it in a decent number of guesses."
+        else:
+            comment = "You finally got it! Maybe next time you'll do even better."
+        print(f"You win!! It took you {guess_count} guesses.")
+        print(comment)
         break
